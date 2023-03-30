39107f14834da218a001edbff8dff7070a0f79cc
fracs31 <pist.fracs95@gmail.com>
2023-03-30 14:44:14 +0200

    ADD style

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
     require_once __DIR__ . "./models/Movie.php"; //includo la classe "Movie"
     $movie1 = new Movie($movies[0]["title"], $movies[0]["original_title"], $movies[0]["country"], $movies[0]["description"], $movies[0]["genres"]); //creo un'istanza di classe "Movie"
     $movie2 = new Movie($movies[1]["title"], $movies[1]["original_title"], $movies[1]["country"], $movies[1]["description"], $movies[1]["genres"]); //creo un'istanza di classe "Movie"
-    var_dump($movie1); //stampa della istanza di classe
-    var_dump($movie2); //stampa della istanza di classe
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,6 +15,52 @@
     <title>PHP OOP1</title>
 </head>
 <body>
-    
+    <!-- Main -->
+    <main class="main-content">
+        <!-- Container -->
+        <div class="container">
+            <!-- Lista dei film -->
+            <ul class="list">
+                <!-- Elemento della lista -->
+                <li class="list-item">
+                    <!-- Carta -->
+                    <div class="card">
+                        <!-- Testa della carta -->
+                        <div class="card__head">
+                        <!-- Immagine della carta -->
+                            <img class="card__image" src="https://www.stardust.it/uploads/img/movie/poster/84a60b3a20d20ec90cacca62641848591555193011-1000w.jpg" alt="Ritorno al futuro">
+                        </div>
+                        <!-- Corpo della carta -->
+                        <div class="card__body">
+                            <!-- Titolo -->
+                            <h2 class="card__title">
+                                Ritorno al futuro
+                            </h2>
+                            <!-- Titolo originale -->
+                            <h3 class="card__original-title">
+                                Back to The Future
+                            </h3>
+                            <!-- Descrizione -->
+                            <h4 class="card__description">
+                                Lorem Ipsum
+                            </h4>
+                            <!-- Paese -->
+                            <h4 class="card__countru">
+                                USA
+                            </h4>
+                            <h4 class="card__genres">
+                                <span>
+                                    Azione
+                                </span>
+                                <span>
+                                    Avventura
+                                </span>
+                            </h4>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </main>
 </body>
 </html>
