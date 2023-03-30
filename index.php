b50ae101c02ce8c9314d61a0b979ef451c3aa381
fracs31 <pist.fracs95@gmail.com>
2023-03-30 14:14:53 +0200

    ADD print of the movie's objects

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<!-- PHP -->
+<?php
+    require_once __DIR__ . "./models/Movie.php"; //includo la classe "Movie"
+    //Lista di film
+    $movie_list = [
+        [
+            "title" => "Ritorno al futuro", //titolo del film
+            "original_title" => "Back to The Future", //titolo originale del film
+            "country" => "USA", //paese del film
+            "description" => "Lorem Ipsum", //descrizione del film
+            "genres" => ["Azione", "Commedia", "Fantascienza"] //generi del film
+        ],
+        [
+            "title" => "Spider-Man", //titolo del film
+            "original_title" => "Spider-Man", //titolo originale del film
+            "country" => "USA", //paese del film
+            "description" => "Lorem Ipsum", //descrizione del film
+            "genres" => ["Azione", "Commedia"] //generi del film
+        ]
+    ];
+    $movie1 = new Movie($movie_list[0]["title"], $movie_list[0]["original_title"], $movie_list[0]["country"], $movie_list[0]["description"], $movie_list[0]["genres"]); //creo un'istanza di classe "Movie"
+    $movie2 = new Movie($movie_list[1]["title"], $movie_list[1]["original_title"], $movie_list[1]["country"], $movie_list[1]["description"], $movie_list[1]["genres"]); //creo un'istanza di classe "Movie"
+    var_dump($movie1); //stampa della istanza di classe
+    var_dump($movie2); //stampa della istanza di classe
+?>
 <!DOCTYPE html>
 <html>
 <head>
